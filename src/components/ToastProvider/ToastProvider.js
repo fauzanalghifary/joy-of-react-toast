141991fe2c3ab6ce289ee15d21336586ae0d20bf
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-01-20 13:25:38 +0700

    init

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([
+    {
+      id: crypto.randomUUID(),
+      message: "It works!",
+      variant: "success",
+    },
+    {
+      id: crypto.randomUUID(),
+      message: "Logged in",
+      variant: "success",
+    },
+  ]);
+
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  function createToast(message, variant) {
+    const nextToasts = [
+      ...toasts,
+      {
+        id: crypto.randomUUID(),
+        message,
+        variant,
+      },
+    ];
+
+    setToasts(nextToasts);
+  }
+
+  function dismissToast(id) {
+    const nextToasts = toasts.filter((toast) => {
+      return toast.id !== id;
+    });
+    setToasts(nextToasts);
+  }
+
+  return (
+    <ToastContext.Provider value={{ toasts, createToast, dismissToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
